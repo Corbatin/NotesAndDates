2b4c61faa74d579511885a48acfe892986e43d2e
Corbatin <84819651+Corbatin@users.noreply.github.com>
2022-12-08 22:03:35 -0300

    Update ControlDeAcceso.py

--- ControlDeAcceso.py
+++ ControlDeAcceso.py
@@ -1,2 +1,14 @@
-def ControlDeAcceso():
-    pass
+def ControlDeAcceso(credencial):
+    SELECT CUE_ID, CUE_PASS, CUE_TUSER
+    FROM CUENTA;
+
+    if(CUE_ID == credencial.username && CUE_PASS = credencial.password && CUE_TUSER == 'A')
+    {
+        return 'Administrador'
+    }
+    if(CUE_ID == credencial.username && CUE_PASS = credencial.password && CUE_TUSER == 'U')
+    {
+        return 'Usuario'
+    }
+    else
+        raise ValueError("Account not found")
