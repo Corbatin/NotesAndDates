99feafebdbbf588a9bc3cf5a434e5444c33db6d9
Corbatin <84819651+Corbatin@users.noreply.github.com>
2022-12-09 20:19:37 -0300

    Control de acceso

--- ControlDeAcceso.py
+++ ControlDeAcceso.py
@@ -1,14 +1,57 @@
-def ControlDeAcceso(credencial):
-    SELECT CUE_ID, CUE_PASS, CUE_TUSER
-    FROM CUENTA;
+import pymysql 
 
-    if(CUE_ID == credencial.username && CUE_PASS = credencial.password && CUE_TUSER == 'A')
-    {
-        return 'Administrador'
-    }
-    if(CUE_ID == credencial.username && CUE_PASS = credencial.password && CUE_TUSER == 'U')
-    {
-        return 'Usuario'
-    }
-    else
-        raise ValueError("Account not found")
+def Database():
+    connection = pymysql.connect(
+        host='localhost',
+        user='root',
+        password='',
+        db='NotesandDates'
+    )
+
+    print("Conexion establecida correctamente")
+
+    mycursor = connection.cursor()
+
+    return mycursor
+
+credencial = ['Admin2BM', 'BMINGSW02', 'A', 'A']
+credencial2 = ['Leaves', 'stick2022', 'U', 'A']
+
+
+def ControlDeAcceso(credencial):
+    db = Database()   
+    sql = 'SELECT * FROM Cuenta WHERE Cuenta.CUE_ID = %s and Cuenta.CUE_PASS = %s and Cuenta.CUE_TUSER = %s and Cuenta.CUE_ESTADO = %s '
+
+    db.execute(sql,credencial)
+    tuser = db.fetchall()
+
+    for user in tuser:
+        print("ID ",user[0]) 
+        print("NOMBRE: ",user[1])   
+        print("TUSER:  ",user[2])  
+
+
+        if credencial[2] == 'A':
+            print("ADMIN")
+            return 'A'
+        if credencial[2] == 'U':
+            print("user") 
+            return 'U'   
+        #for atts in user:
+        #    print("epico")
+        #    if credencial[0] == user[0] and credencial[1] == user[1] and credencial[2] == 'A' and credencial[3] == 'A':
+        #
+        #        print("Usuario tipo Admin")
+        #        return 'A'
+        #
+        #    if credencial[0] == user[0] and credencial[1] == user[1] and credencial[2] == 'U':
+        #        
+        #        print("Usuario tipo usuario")
+        #        return 'U'    
+            
+
+     
+
+
+
+ControlDeAcceso(credencial2)     
