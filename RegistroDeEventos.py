1c8fc72ed381e03d9ce37bc7f64d8b574e84a6f1
FDiazVallejos <120508426+FDiazVallejos@users.noreply.github.com>
2022-12-13 23:06:34 -0300

    Update RegistroDeEventos.py

--- RegistroDeEventos.py
+++ RegistroDeEventos.py
@@ -1,4 +1,5 @@
 import pymysql
+import sys
 
 def Database():
     connection = pymysql.connect(
@@ -7,24 +8,13 @@ def Database():
         password='',
         db='NotesandDates'
     )
+    
+print("Conexion establecida correctamente")
+sys.stdout.flush()
+mycursor = connection.cursor()
+return [mycursor, connection]
 
-    print("Conexion establecida correctamente")
-
-    mycursor = connection.cursor()
-
-    return [mycursor, connection]
-
-
-evento = ['17', 'Leaves', 'V', 'REU', '1', '2023', 'Webinar de la empresa', 'El enlace será enviado en las proximas horas', '0900', '1000' ]
-
-
-def RegistroDeEventos(evento):
-
-    db = Database()
-
-
-    sql = 'INSERT INTO Evento VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
-    db[0].execute(sql,evento)
-    db[1].commit()
-
-RegistroDeEventos(evento)
+db = Database()
+sql = 'INSERT INTO Evento VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+db[0].execute(sql,evento)
+db[1].commit()
