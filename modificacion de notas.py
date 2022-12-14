43650aa931e7b95e2cb3a540e1beafb6e03f4975
FDiazVallejos <120508426+FDiazVallejos@users.noreply.github.com>
2022-12-13 23:10:33 -0300

    Update modificacion de notas.py

--- modificacion de notas.py
+++ modificacion de notas.py
@@ -1,4 +1,5 @@
 import pymysql
+import sys
 
 def Database():
     connection = pymysql.connect(
@@ -8,23 +9,15 @@ def Database():
         db='NotesandDates'
     )
 
-    print("Conexion establecida correctamente")
+print("Conexion establecida correctamente")
+sys.stdout.flush()
+mycursor = connection.cursor()
+return [mycursor, connection]
 
-    mycursor = connection.cursor()
+db = Database()
+actualizacion = [nota[2], nota[3], nota[0], nota[1]]
+sql = 'UPDATE Nota SET NOT_NOMBRE = %s, NOT_DESCRIPCION = %s WHERE NOT_ID = %s AND NOT_CUENTA= %s'
+db[0].execute(sql, actualizacion)
+db[1].commit()
 
-    return [mycursor, connection]
-
-nota1= ['1', 'Leaves', 'fumar lucky', 'Tocar con pipazp épico' ]
-
-def modificacion_notas(nota):
-    db = Database()
-
-    actualizacion = [nota[2], nota[3], nota[0], nota[1]]
-    
-    
-    
-    sql = 'UPDATE Nota SET NOT_NOMBRE = %s, NOT_DESCRIPCION = %s WHERE NOT_ID = %s AND NOT_CUENTA= %s'
-    db[0].execute(sql, actualizacion)
-    db[1].commit()
-
-modificacion_notas(nota1)
+sys.stdout.flush()
