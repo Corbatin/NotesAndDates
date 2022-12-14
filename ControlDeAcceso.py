0ea32f4a86ceb32617e3963a5cfc12f9a9c29441
FDiazVallejos <120508426+FDiazVallejos@users.noreply.github.com>
2022-12-13 22:54:56 -0300

    Update ControlDeAcceso.py

--- ControlDeAcceso.py
+++ ControlDeAcceso.py
@@ -15,25 +15,24 @@ def Database():
 
     return mycursor
 
+db = Database()   
+sql = 'SELECT * FROM Cuenta WHERE Cuenta.CUE_ID = %s and Cuenta.CUE_PASS = %s and Cuenta.CUE_TUSER = %s and Cuenta.CUE_ESTADO = %s '
 
-    db = Database()   
-    sql = 'SELECT * FROM Cuenta WHERE Cuenta.CUE_ID = %s and Cuenta.CUE_PASS = %s and Cuenta.CUE_TUSER = %s and Cuenta.CUE_ESTADO = %s '
+db.execute(sql,credencial)
+tuser = db.fetchall()
 
-    db.execute(sql,credencial)
-    tuser = db.fetchall()
-
-    for user in tuser:
-        print("ID ",user[0]) 
-        print("NOMBRE: ",user[1])   
-        print("TUSER:  ",user[2])  
+for user in tuser:
+    print("ID ",user[0]) 
+    print("NOMBRE: ",user[1])   
+    print("TUSER:  ",user[2])  
 
 
-        if credencial[2] == 'A':
-            print("ADMIN")
-            return 'A'
-        if credencial[2] == 'U':
-            print("user") 
-            return 'U'   
+    if credencial[2] == 'A':
+        print("ADMIN")
+        return 'A'
+    if credencial[2] == 'U':
+        print("user") 
+        return 'U'   
         #for atts in user:
         #    print("epico")
         #    if credencial[0] == user[0] and credencial[1] == user[1] and credencial[2] == 'A' and credencial[3] == 'A':
