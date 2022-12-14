775b2cd0e8f4b57930819aaed888b7e2aae1f276
FDiazVallejos <120508426+FDiazVallejos@users.noreply.github.com>
2022-12-13 23:09:26 -0300

    Update modificacion de evento.py

--- modificacion de evento.py
+++ modificacion de evento.py
@@ -1,4 +1,5 @@
 import pymysql
+import sys
 
 def Database():
     connection = pymysql.connect(
@@ -8,17 +9,17 @@ def Database():
         db='NotesandDates'
     )
 
-    print("Conexion establecida correctamente")
+print("Conexion establecida correctamente")
+sys.stdout.flush()
 
-    mycursor = connection.cursor()
+mycursor = connection.cursor()
+return [mycursor, connection]
 
-    return [mycursor, connection]
 
-def modificacion_evento(evento):
-    db = Database()
+db = Database()
+actualizacion = [evento[2], evento[3], evento[4], evento[5], evento[6], evento[7], evento[8], evento[9], evento[0]]
+sql = 'UPDATE Evento SET EVE_MOD = %s, EVE_TIPO = %s, EVE_DNUM = %s, EVE_DANYO = %s, EVE_NOMBRE = %s, EVE_DESCRIPCION = %s, EVE_HORAINICIO = %s, EVE_HORAFIN = %s WHERE EVE_ID = %s' 
+db[0].execute(sql, actualizacion)
+db[1].commit()
 
-    actualizacion = [evento[2], evento[3], evento[4], evento[5], evento[6], evento[7], evento[8], evento[9], evento[0]]
-    sql = 'UPDATE Evento SET EVE_MOD = %s, EVE_TIPO = %s, EVE_DNUM = %s, EVE_DANYO = %s, EVE_NOMBRE = %s, EVE_DESCRIPCION = %s, EVE_HORAINICIO = %s, EVE_HORAFIN = %s WHERE EVE_ID = %s' 
-    db[0].execute(sql, actualizacion)
-    db[1].commit()
-
+sys.stdout.flush()
