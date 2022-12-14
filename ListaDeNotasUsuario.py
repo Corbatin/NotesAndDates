8d85d04ddf09e385f3d78234d44cb94d6ca983dd
FDiazVallejos <120508426+FDiazVallejos@users.noreply.github.com>
2022-12-13 23:03:59 -0300

    Update ListaDeNotasUsuario.py

--- ListaDeNotasUsuario.py
+++ ListaDeNotasUsuario.py
@@ -1,4 +1,5 @@
 import pymysql
+import sys
 
 def Database():
     connection = pymysql.connect(
@@ -9,28 +10,19 @@ def Database():
     )
 
     print("Conexion establecida correctamente")
+    sys.stdout.flush()
 
     mycursor = connection.cursor()
-
     return mycursor
 
-U_ID = 'Leaves'
-
-def gListaNotaUsuario(U_ID):
-
-    db = Database()
-
-
-    sql = 'SELECT * FROM Nota,Cuenta WHERE Cuenta.CUE_ID = Nota.NOT_CUENTA AND NOT_CUENTA = %s'
-    db.execute(sql, U_ID)
-    nota = db.fetchall()
-
-
-    return nota
-
-lista = gListaNotaUsuario(U_ID)
+db = Database()
+sql = 'SELECT * FROM Nota,Cuenta WHERE Cuenta.CUE_ID = Nota.NOT_CUENTA AND NOT_CUENTA = %s'
+db.execute(sql, U_ID)
+nota = db.fetchall()
 
 for nota in lista:
-        print("Id", nota[0])
-        print("Cuenta", nota[1])
-        print("Nombre Nota", nota[2])
+    print("Id", nota[0])
+    print("Cuenta", nota[1])
+    print("Nombre Nota", nota[2])
+    
+sys.stdout.flush()
