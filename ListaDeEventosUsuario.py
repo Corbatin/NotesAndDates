46e0913260b708e83a85310514beee7cf794f3cd
FDiazVallejos <120508426+FDiazVallejos@users.noreply.github.com>
2022-12-13 23:02:15 -0300

    Update ListaDeEventosUsuario.py

--- ListaDeEventosUsuario.py
+++ ListaDeEventosUsuario.py
@@ -1,4 +1,5 @@
 import pymysql
+import sys
 
 def Database():
     connection = pymysql.connect(
@@ -9,29 +10,21 @@ def Database():
     )
 
     print("Conexion establecida correctamente")
+    sys.stdout.flush()
 
     mycursor = connection.cursor()
-
     return mycursor
 
-U_ID = 'Leaves'
 
-def gListaEventoUsuario(U_ID):
-
-    db = Database()
-
-
-    sql = 'SELECT * FROM Evento,Cuenta WHERE Cuenta.CUE_ID = Evento.EVE_CUENTA AND EVE_CUENTA = %s'
-    db.execute(sql, U_ID)
-    evento = db.fetchall()
-
-
-    return evento
-
-lista = gListaEventoUsuario(U_ID)
+db = Database()
+sql = 'SELECT * FROM Evento,Cuenta WHERE Cuenta.CUE_ID = Evento.EVE_CUENTA AND EVE_CUENTA = %s'
+db.execute(sql, U_ID)
+evento = db.fetchall()
 
 for evento in lista:
         print("Id", evento[0])
         print("Cuenta", evento[1])
         print("Nombre Evento", evento[6])
         print("Descripcion Evento", evento[7])
+
+sys.stdout.flush()        
